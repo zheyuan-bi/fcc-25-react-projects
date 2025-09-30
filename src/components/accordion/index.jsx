86a5aaab47a2f3d10fed8ea6643b0a749eae1932
zheyuan <bizheyuan2000@icloud.com>
2025-09-30 16:11:26 +0800

    finish single/multi select accordion

---
+++ src/components/accordion/index.jsx
@@ -0,0 +1,47 @@
+// single selection
+
+// multiple selection
+
+import data from "./data";
+import { useState } from "react";
+import "./styles.css";
+
+export default function Accordion() {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelect, setEnableMultiSelect] = useState(false);
+  const [multipleSelectedIds, setMultipleSelectedIds] = useState([]);
+
+  console.log(selected, multipleSelectedIds, enableMultiSelect);
+
+  function handleSingleSelection(id) {
+    console.log("single");
+    id === selected ? setSelected(null) : setSelected(id);
+  }
+
+  function handleMultiSelection(id) {
+    setMultipleSelectedIds((prev) => (prev.includes(id) ? prev.filter((i) => i !== id) : [...prev, id]));
+  }
+
+  return (
+    <div className="wrapper">
+      <button onClick={() => setEnableMultiSelect(!enableMultiSelect)}>enable multi selection</button>
+      <div className="accordion">
+        {data.map((item) => (
+          <div className="item" key={item.id}>
+            <div
+              onClick={() => (enableMultiSelect ? handleMultiSelection(item.id) : handleSingleSelection(item.id))}
+              className="title"
+            >
+              <h3>{item.question}</h3>
+              <span>+</span>
+            </div>
+            {(enableMultiSelect && multipleSelectedIds.includes(item.id)) ||
+            (!enableMultiSelect && selected === item.id) ? (
+              <div className="content">{item.answer}</div>
+            ) : null}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
